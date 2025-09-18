b92ebc65990adbfc33d91d307c177dca9565a599
ManviSinghal12 <singhalmanvi1206@gmail.com>
2025-09-18 23:11:25 +0530

    Update login.jsx
    for login page in mid

--- src/Features/login.jsx
+++ src/Features/login.jsx
@@ -23,10 +23,22 @@ const Login = () => {
     return (
         <div className="login">
         <div className="login-page">
-            <div className="login-container" data-aos="zoom-in">
+            <div className="login-container" data-aos="zoom-in"<a href="#" className="back-to-home">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-arrow-left"
+          > 
                 <Link to="/" className="back-to-home">
-                    <i data-feather="arrow-left"></i> Back to Home
-                </Link>
+                    <i data-feather="arrow-left"></i> </svg> Back to Home
+                </Link> </a>
 
                 <div className="login-header">
                     <img src={logoImage} alt="Krishi Sahayak Logo" className="login-logo" />
@@ -36,10 +48,39 @@ const Login = () => {
 
                 <form className="login-form" onSubmit={handleFormSubmit}>
                     <div className="input-group">
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="input-icon feather feather-mail"
+            >
+                          <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                          <polyline points="22,6 12,13 2,6"></polyline>
+            </svg>
                         <i data-feather="mail" className="input-icon"></i>
                         <input type="email" placeholder="Email Address" required />
                     </div>
                     <div className="input-group">
+                        <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         width="20"
+                         height="20"
+                         viewBox="0 0 24 24"
+                         fill="none"
+                         stroke="currentColor"
+                         strokeWidth="2"
+                         strokeLinecap="round"
+                         strokeLinejoin="round"
+                         className="input-icon feather feather-lock"
+            >
+                         <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                         <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                         </svg>
                         <i data-feather="lock" className="input-icon"></i>
                         <input type="password" placeholder="Password" required />
                     </div>
@@ -57,8 +98,15 @@ const Login = () => {
                     <p>Don't have an account? Click on Register Button </p>
                 </div>
             </div>
+            {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <p>{message}</p>
+            <button onClick={closeModal}>OK</button>
+          </div>
         </div>
-        </div>
+      )}
+        
     );
 };
 
